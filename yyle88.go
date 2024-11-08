f825800928726cf04bcb5d529972fda1ed54134a
yangyile <example@gogit.com>
2024-11-08 20:05:51 +0700

    再把文档写写

--- yyle88.go
+++ yyle88.go
@@ -1,1 +1,42 @@
 package yyle88
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-resty/resty/v2"
+	"github.com/yyle88/done"
+	"github.com/yyle88/must"
+	"github.com/yyle88/neatjson/neatjsons"
+	"github.com/yyle88/sortslice"
+	"github.com/yyle88/zaplog"
+)
+
+type Repo struct {
+	Name       string    `json:"name"`
+	Link       string    `json:"html_url"`
+	Desc       string    `json:"description"`
+	Stargazers int       `json:"stargazers_count"`
+	PushedAt   time.Time `json:"pushed_at"`
+}
+
+func MustGetGithubRepos(username string) []*Repo {
+	var repos []*Repo
+	resp := done.VPE(resty.New().SetTimeout(time.Minute).R().
+		SetPathParam("username", username).
+		SetResult(&repos).
+		Get("https://api.github.com/users/{username}/repos")).Nice()
+	must.Equals(resp.StatusCode(), http.StatusOK)
+
+	sortslice.SortVStable(repos, func(a, b *Repo) bool {
+		if a.Stargazers > b.Stargazers {
+			return true
+		} else if a.Stargazers < b.Stargazers {
+			return false
+		} else {
+			return a.PushedAt.After(b.PushedAt)
+		}
+	})
+	zaplog.SUG.Info(neatjsons.S(repos))
+	return repos
+}
--- yyle88_test.go
+++ yyle88_test.go
@@ -1,13 +1,20 @@
 package yyle88_test
 
 import (
+	"fmt"
 	"os"
+	"slices"
+	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/require"
 	"github.com/yyle88/done"
 	"github.com/yyle88/must"
 	"github.com/yyle88/osexistpath/osmustexist"
+	"github.com/yyle88/runpath"
 	"github.com/yyle88/runpath/runtestpath"
+	"github.com/yyle88/yyle88"
+	"github.com/yyle88/yyle88/internal/utils"
 )
 
 func TestExample(t *testing.T) {
@@ -20,4 +27,41 @@ func TestExample(t *testing.T) {
 	data := done.VAE(os.ReadFile(path)).Nice()
 
 	t.Log(string(data))
+}
+
+func TestGenMarkdown(t *testing.T) {
+	username := "yyle88"
+	repos := yyle88.MustGetGithubRepos(username)
+
+	ptx := utils.NewPTX()
+	ptx.Println("| 项目名称 | 项目描述 |")
+	ptx.Println("|-------------------------------------------------|--------|")
+	for _, repo := range repos {
+		ptx.Println(fmt.Sprintf("| [%s](%s) | %s |", repo.Name, repo.Link, strings.ReplaceAll(repo.Desc, "|", "-")))
+	}
+
+	stb := ptx.String()
+	t.Log(stb)
+
+	path := osmustexist.PATH(runpath.PARENT.Join("README.md"))
+	t.Log(path)
+
+	text := string(done.VAE(os.ReadFile(path)).Nice())
+	t.Log(text)
+
+	sLns := strings.Split(text, "\n")
+	sIdx := slices.Index(sLns, "这是我的项目：")
+	require.Positive(t, sIdx)
+	eIdx := slices.Index(sLns, "给我星星谢谢。")
+	require.Positive(t, eIdx)
+
+	require.Less(t, sIdx, eIdx)
+
+	content := strings.Join(sLns[:sIdx+1], "\n") + "\n" + "\n" +
+		stb + "\n" +
+		strings.Join(sLns[eIdx:], "\n")
+	t.Log(content)
+
+	require.NoError(t, os.WriteFile(path, []byte(content), 0666))
+	t.Log("success")
 }
