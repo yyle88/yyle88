f745b4b6ae7f60bead662805b9467aaefbdb2cb0
yangyile <example@gogit.com>
2024-12-16 22:30:20 +0800

    增加链接

--- yyle88_test.go
+++ yyle88_test.go
@@ -22,7 +22,7 @@ type DocGenParam struct {
 	shortName string
 	startWith string
 	titleLine string
-	otherDesc string
+	//otherDesc string
 	closeWith string
 }
 
@@ -33,7 +33,7 @@ func TestGenMarkdown(t *testing.T) {
 		shortName: "README.md",
 		startWith: "Here are some of my key projects:",
 		titleLine: "| **Repo Name** | **Description** |",
-		otherDesc: "OTHER-PROJECTS:",
+		//otherDesc: "OTHER-PROJECTS:",
 		closeWith: "**Explore and star my projects. Your support means a lot!**",
 	})
 }
@@ -45,7 +45,7 @@ func TestGenMarkdownZhHans(t *testing.T) {
 		shortName: "README.zh.md",
 		startWith: "这是我的项目：",
 		titleLine: "| 项目名称 | 项目描述 |",
-		otherDesc: "其它项目：",
+		//otherDesc: "其它项目：",
 		closeWith: "给我星星谢谢。",
 	})
 }
@@ -55,11 +55,19 @@ func GenMarkdownTable(t *testing.T, username string, arg *DocGenParam) {
 
 	ptx := utils.NewPTX()
 
+	// collect themes from: https://github.com/anuraghazra/github-readme-stats/blob/0810a1d8446902f0267a151302388e5ed8373aa2/themes/README.md?plain=1#L50
+	cardThemes := []string{"default_repocard", "transparent", "shadow_red", "shadow_green", "shadow_blue", "dark", "radical", "merko", "gruvbox", "gruvbox_light", "tokyonight", "onedark", "cobalt", "synthwave", "highcontrast", "dracula", "prussian", "monokai", "vue", "vue-dark", "shades-of-purple", "nightowl", "buefy", "blue-green", "algolia", "great-gatsby", "darcula", "bear", "solarized-dark", "solarized-light", "chartreuse-dark", "nord", "gotham", "material-palenight", "graywhite", "vision-friendly-dark", "ayu-mirage", "midnight-purple", "calm", "flag-india", "omni", "react", "jolly", "maroongold", "yeblu", "blueberry", "slateorange", "kacho_ga", "outrun", "ocean_dark", "city_lights", "github_dark", "github_dark_dimmed", "discord_old_blurple", "aura_dark", "panda", "noctis_minimus", "cobalt2", "swift", "aura", "apprentice", "moltack", "codeSTACKr", "rose_pine", "catppuccin_latte", "catppuccin_mocha", "date_night", "one_dark_pro", "rose", "holi", "neon", "blue_navy", "calm_pink", "ambient_gradient"}
+
 	subRepos, repos := splitRepos(repos, 5)
 	for _, repo := range subRepos {
-		const templateLine = "[![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username={{ username }}&repo={{ repo_name }}&theme=algolia)]({{ repo_link }})"
+		const templateLine = "[![Readme Card](https://github-readme-stats.vercel.app/api/pin/?username={{ username }}&repo={{ repo_name }}&theme={{ card_theme }})]({{ repo_link }})"
 
-		rep := strings.NewReplacer("{{ username }}", username, "{{ repo_name }}", repo.Name, "{{ repo_link }}", repo.Link)
+		rep := strings.NewReplacer(
+			"{{ username }}", username,
+			"{{ repo_name }}", repo.Name,
+			"{{ card_theme }}", cardThemes[rand.IntN(len(cardThemes))],
+			"{{ repo_link }}", repo.Link,
+		)
 
 		ptx.Println(rep.Replace(templateLine))
 		ptx.Println()
@@ -83,9 +91,21 @@ func GenMarkdownTable(t *testing.T, username string, arg *DocGenParam) {
 
 	if len(repos) > 0 {
 		ptx.Println()
-		ptx.Println(arg.otherDesc)
-		for idx, repo := range repos {
-			ptx.Println(makeBadge(repo, colors[idx%len(colors)]))
+		//ptx.Println(arg.otherDesc)
+		//ptx.Println()
+		const stepLimit = 4
+		ptx.Println("|" + repeatString(" |", stepLimit))
+		ptx.Println("|" + repeatString(" :--: |", stepLimit))
+		for start := 0; start < len(repos); start += stepLimit {
+			ptx.Print("|")
+			for num := 0; num < stepLimit; num++ {
+				if idx := start + num; idx < len(repos) {
+					ptx.Print(makeBadge(repos[idx], colors[idx%len(colors)]), " | ")
+				} else {
+					ptx.Print("-", " | ")
+				}
+			}
+			ptx.Println()
 		}
 		ptx.Println()
 	}
@@ -123,4 +143,12 @@ func splitRepos(repos []*yyle88.Repo, subSize int) ([]*yyle88.Repo, []*yyle88.Re
 
 func makeBadge(repo *yyle88.Repo, colorString string) string {
 	return fmt.Sprintf("[![%s](https://img.shields.io/badge/%s-%s.svg?style=flat&logoColor=white)](%s)", repo.Name, repo.Name, url.QueryEscape(colorString), repo.Link)
+}
+
+func repeatString(s string, n int) string {
+	var res string
+	for i := 0; i < n; i++ {
+		res += s
+	}
+	return res
 }
